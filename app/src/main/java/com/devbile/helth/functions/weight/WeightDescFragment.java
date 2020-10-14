edc02cc742e2254569906cff480700675415bd62
fexxy3112 <mateusz.cimachowski@gmail.com>
2020-10-14 17:21:50 +0200

    Dizajn Weight

---
+++ app/src/main/java/com/devbile/helth/functions/weight/WeightDescFragment.java
@@ -0,0 +1,32 @@
+package com.devbile.helth.functions.weight;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import com.devbile.helth.R;
+import com.devbile.helth.SecondFragment;
+
+public class WeightDescFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weight_desc, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(WeightCfgStartFragment.this, "Mój Profil", Toast.LENGTH_SHORT).show();
+                System.out.println("CLICK");
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/devbile/helth/functions/weight/WeightFormFragment.java
+++ app/src/main/java/com/devbile/helth/functions/weight/WeightFormFragment.java
@@ -4,18 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-import com.devbile.helth.MainActivity;
 import com.devbile.helth.R;
 
-public class WeightCfgStartFragment extends Fragment {
-
+public class WeightFormFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weight_cfg_start, container, false);
+        return inflater.inflate(R.layout.fragment_weight_desc, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
